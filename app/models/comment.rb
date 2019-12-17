84c45492a0c40953746cbc19a6a08978e2071f5a
ReiShibuya <dir_en_grey_shibuzo@yahoo.co.jp>
2019-12-17 22:59:57 +0900

    default name is nanashisan

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :topic
   validates :name, presence: true,  length: { minimum: 3 }
   validates :content, presence: true, length: { minimum: 3 }
+  before_validation :set_nameless_nanashi
+
+  private
+    def set_nameless_nanashi
+      self.name = '名無しさん' if name.blank?
+    end
 
 end
